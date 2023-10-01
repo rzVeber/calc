8d48cba6d47ca78f6ccf9c2519edeaaa39eb7382
Roman Weber <3skirt@gmail.com>
2023-10-01 19:12:52 +0300

    revactor trying

--- src/main/java/org/calc/Funcs.java
+++ src/main/java/org/calc/Funcs.java
@@ -2,38 +2,31 @@ package org.calc;
 
 public class Funcs {
     public static float sum(int a, int b){ // +
-        return (float)a+(float)b;
+        return sum((float)a,(float)b);
     }
     public static float sum(float a, float b){ // +
         return a+b;
     }
     public static float sub(int a, int b){ // -
-        return (float)a-(float)b;
+        return sub((float)a,(float)b);
     }
     public static float sub(float a, float b){ // -
         return a-b;
     }
     public static float mul(int a, int b){ // *
-        return (float)a*(float)b;
+        return mul((float)a,(float)b);
     }
     public static float mul(float a, float b){ // *
         return a*b;
     }
     public static float div(int a, int b){ // /
-        float c;
-        if(a==0 && b==0){
-            return 0;
-        } else if(a!=0 || b==0) {
-            return Float.MAX_VALUE;
-        }
-        c=a/b;
-        return c;
+        return div((float)a, (float)b);
     }
     public static float div(float a, float b){ // /
         float c;
-        if(a==0.0f && b==0.0f){
+        if(a==0.0f && b==0.0f){ //если первое число ноль то деление невозможно, представлено минимально возможным числом (нулем)
             return 0;
-        } else if(a!=0.0f && b==0.0f){
+        } else if(a!=0.0f && b==0.0f){ //если происходит деление на ноль выходит бесконечность, её представить невозможно и заменяется ближайшим лимитом
             return Float.MAX_VALUE;
         }
         c=a/b;
