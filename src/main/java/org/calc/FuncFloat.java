c6fe464cd3702090cf4faa705407f95cf6c19d2e
Roman Weber <3skirt@gmail.com>
2023-10-01 19:44:10 +0300

    refactor struct

--- src/main/java/org/calc/FuncFloat.java
+++ src/main/java/org/calc/FuncFloat.java
@@ -1,26 +1,15 @@
 package org.calc;
 
-public class Funcs {
-    public static float sum(int a, int b){ // +
-        return sum((float)a,(float)b);
-    }
+public class FuncFloat{
     public static float sum(float a, float b){ // +
         return a+b;
     }
-    public static float sub(int a, int b){ // -
-        return sub((float)a,(float)b);
-    }
+
     public static float sub(float a, float b){ // -
         return a-b;
     }
-    public static float mul(int a, int b){ // *
-        return mul((float)a,(float)b);
-    }
     public static float mul(float a, float b){ // *
         return a*b;
-    }
-    public static float div(int a, int b){ // /
-        return div((float)a, (float)b);
     }
     public static float div(float a, float b){ // /
         float c;
